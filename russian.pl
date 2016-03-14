49b609ab669d6b3ed685ba1b8459cca94cf925da
Abraao Mota <abraaomot@outlook.com>
2016-03-14 20:25:05 +0000

    Initial comments
    Signed-off-by: Abraao Mota <abraaomot@outlook.com>

---
+++ russian.pl
@@ -0,0 +1,49 @@
+% prolog
+
+% This is a little tool to help with learning russian, prolog style.
+
+
+% The methods take a list of letters (a word) and output the correspondent output
+
+
+% Due to coding limitations and language barriers, output and input will follow a phonetic approach, 
+% using the following mapping
+
+/*
+
+
+а = a
+б = b
+в = v
+г = g
+д = d
+е = ye
+ё = yo
+ж = xh
+з = z
+и = i
+й = y
+к = k
+л = l
+м = m
+н = n
+о = o
+п = p
+р = r
+с = s
+т = t
+у = u
+ф = f
+х = h
+ц = ts
+ч = ch
+ш = sh
+щ = tsch
+ъ = _ ??
+ы = ui
+ь = `
+э = e
+ю = yu
+я = ya
+
+*/
