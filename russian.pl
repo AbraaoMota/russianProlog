065c20c0bbe07d7e07c8c3f96018abb562e8e5e9
Abraao Mota <abraaomot@outlook.com>
2016-03-14 22:42:46 +0000

    Move comments
    Signed-off-by: Abraao Mota <abraaomot@outlook.com>

--- russian.pl
+++ russian.pl
@@ -3,63 +3,6 @@
 % prolog
 
 
-
-% This is a little tool to help with learning russian, prolog style.
-
-
-% The methods take a list of letters (a word) and output the correspondent output
-
-
-% Due to coding limitations and language barriers, output and input will follow a phonetic approach, 
-% using the following mapping
-
-/*
-
-
-а = a
-б = b
-в = v
-г = g
-д = d
-е = ye
-ё = yo
-ж = xh
-з = z
-и = i
-й = y
-к = k
-л = l
-м = m
-н = n
-о = o
-п = p
-р = r
-с = s
-т = t
-у = u
-ф = f
-х = h
-ц = ts
-ч = ch
-ш = sh
-щ = tsch
-ъ = _ ??
-ы = ui
-ь = `
-э = e
-ю = yu
-я = ya
-
-*/
-
-
-%% add_to_end([X|Xs], InitCopy, NewLetter, Intermediate, NewWord) :-
-%% 	Ending = [X|NewLetter],
-%% 	NewWord = [Intermediate|Ending].
-%% add_to_end([X|Xs], InitCopy, , NewLetter, Intermediate, NewWord) :-
-%% 	add_to_end([X|Xs], [Xs], NewLetter, [X|Intermediate], NewWord).
-
-
 add_to_end([X], NewLetter, Intermediate, NewWord) :-
 	Int2 = [NewLetter, X],
 	append(Int2, Intermediate, Int3),
